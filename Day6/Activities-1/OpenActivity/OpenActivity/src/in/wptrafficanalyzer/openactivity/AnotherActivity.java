bdfeceae326d3f4d266c6cb1b08a7b99ad627f7c
Chaitanya Bommaraju <chaitanya.bommaraju@gmail.com>
2016-02-18 10:37:30 +0530

    Added examples on ListView with checkbox,radio and event handling  plus Activities

---
+++ Day6/Activities-1/OpenActivity/OpenActivity/src/in/wptrafficanalyzer/openactivity/AnotherActivity.java
@@ -0,0 +1,12 @@
+package in.wptrafficanalyzer.openactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AnotherActivity  extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.another);
+	}
+}
